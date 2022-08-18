3424ca222006d23893f4e13a326aa709ab889775
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-08-17 19:57:32 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
+import image from './assets/images/background1.jpg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -13,8 +13,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    color: 'black',
+    fontSize: 40,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+  },
+
 });
